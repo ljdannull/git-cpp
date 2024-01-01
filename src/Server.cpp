40381b8a71957cd3d54cd1fd7370846a37a6a828
Lukas Dannull <ld237@duke.edu>
2024-01-01 18:44:36 -0500

    pass 1st stage

--- src/Server.cpp
+++ src/Server.cpp
@@ -3,8 +3,66 @@
 #include <fstream>
 #include <string>
 #include <zlib.h>
+#include <cstdio>
 
 #define CHUNK 16384
+int inf(FILE *source, FILE *dest)
+{
+    int ret;
+    unsigned have;
+    z_stream strm;
+    unsigned char in[CHUNK];
+    unsigned char out[CHUNK];
+
+    /* allocate inflate state */
+    strm.zalloc = Z_NULL;
+    strm.zfree = Z_NULL;
+    strm.opaque = Z_NULL;
+    strm.avail_in = 0;
+    strm.next_in = Z_NULL;
+    ret = inflateInit(&strm);
+    if (ret != Z_OK)
+        return ret;
+
+    /* decompress until deflate stream ends or end of file */
+    do {
+        strm.avail_in = fread(in, 1, CHUNK, source);
+        if (ferror(source)) {
+            (void)inflateEnd(&strm);
+            return Z_ERRNO;
+        }
+        if (strm.avail_in == 0)
+            break;
+        strm.next_in = in;
+
+        /* run inflate() on input until output buffer not full */
+        do {
+            strm.avail_out = CHUNK;
+            strm.next_out = out;
+            ret = inflate(&strm, Z_NO_FLUSH);
+            assert(ret != Z_STREAM_ERROR);  /* state not clobbered */
+            switch (ret) {
+            case Z_NEED_DICT:
+                ret = Z_DATA_ERROR;     /* and fall through */
+            case Z_DATA_ERROR:
+            case Z_MEM_ERROR:
+                (void)inflateEnd(&strm);
+                return ret;
+            }
+            have = CHUNK - strm.avail_out;
+            if (fwrite(out, 1, have, dest) != have || ferror(dest)) {
+                (void)inflateEnd(&strm);
+                return Z_ERRNO;
+            }
+        } while (strm.avail_out == 0);
+
+        /* done when inflate() says it's done */
+    } while (ret != Z_STREAM_END);
+
+    /* clean up and return */
+    (void)inflateEnd(&strm);
+    return ret == Z_STREAM_END ? Z_OK : Z_DATA_ERROR;
+}
 
 int main(int argc, char* argv[]) {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -43,56 +101,10 @@ int main(int argc, char* argv[]) {
         }
         try {
             std::string blob_sha = argv[4];
-            std::ifstream blob_file(".git/objects/" + blob_sha.insert(2, "/"), std::ios::binary);
-            if (!blob_file.is_open()) {
-                std::cerr << "Failed to find blob.\n";
-                return EXIT_FAILURE;
-            }
-            int ret, flush;
-            unsigned have;
-            z_stream strm;
-            unsigned char in[CHUNK];
-            unsigned char out[CHUNK];
-            strm.zalloc = Z_NULL;
-            strm.zfree = Z_NULL;
-            strm.opaque = Z_NULL;
-            strm.avail_in = 0;
-            strm.next_in = Z_NULL;
-            ret = inflateInit(&strm);
-            if (ret != Z_OK)
-                return ret;
+            FILE* blob_file = fopen(".git/objects/" + blob_sha.insert(2, "/"), std::ios::binary, "r");
+            FILE* customStdout = fdopen(1, "w");
+            inf(blob_file, customStdout);
             
-            while (blob_file.read((char*)(&in[0]), CHUNK)) {
-                do {
-                    strm.avail_in = fread(in, 1, CHUNK, source);
-                    if (ferror(source)) {
-                        (void)inflateEnd(&strm);
-                        return Z_ERRNO;
-                    }
-                    if (strm.avail_in == 0)
-                        break;
-                    strm.next_in = in;
-                    do {
-                        strm.avail_out = CHUNK;
-                        strm.next_out = out;
-                        ret = inflate(&strm, Z_NO_FLUSH);
-                        assert(ret != Z_STREAM_ERROR);  /* state not clobbered */
-                        switch (ret) {
-                        case Z_NEED_DICT:
-                            ret = Z_DATA_ERROR;     /* and fall through */
-                        case Z_DATA_ERROR:
-                        case Z_MEM_ERROR:
-                            (void)inflateEnd(&strm);
-                            return ret;
-                        }
-                    
-                        have = CHUNK - strm.avail_out;
-                        std::cout.write(out, CHUNK);
-                    } while (strm.avail_out == 0);
-                } while (ret != Z_STREAM_END);
-                (void)inflateEnd(&strm);
-                return ret == Z_STREAM_END ? Z_OK : Z_DATA_ERROR;
-            }
 
         } catch (const std::filesystem::filesystem_error& e) {
             std::cerr << e.what() << '\n';
@@ -107,3 +119,4 @@ int main(int argc, char* argv[]) {
     
     return EXIT_SUCCESS;
 }
+
